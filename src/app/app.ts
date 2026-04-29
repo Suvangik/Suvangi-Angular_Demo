f5be0e72cd116c1e6372c7e12057619d0d78ef30
Suvangik <suvangikumari1999@gmail.com>
2026-04-29 12:47:03 +0530

    Add files via upload

---
+++ src/app/app.ts
@@ -0,0 +1,22 @@
+// src/app/app.ts
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIf],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class AppComponent {
+  title = 'Appointment Booking App';
+
+  // this fixes: Property 'currentYear' does not exist on type 'AppComponent'
+  currentYear = new Date().getFullYear();
+
+  // simple flag so NgIf is actually used (removes warning NG8113)
+  showHeader = true;
+}
+
